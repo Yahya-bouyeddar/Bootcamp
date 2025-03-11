866c6ba26c82ef7acb9e9a5bd67a6836e674a960
unknown <ybouazzani@gmail.com>
2025-03-11 12:29:38 +0100

    daily challenge gold

---
+++ Week1/Day1/DailyChallengeExo/exerciceDaily GOLDChallenge3.py
@@ -0,0 +1,25 @@
+
+
+année_de_naissance = int(input('Pourriez-vous me donner votre année de naissance'))
+date_de_naissance = input('donner moi date de naissance en forme de DD/MM/YYYY')
+
+
+
+age = 2025-année_de_naissance
+print(age)
+bougie = str(age)[-1]
+print(bougie)
+
+
+mot = ""
+for i in range(int(bougie)):
+    mot += "i"
+
+print(f"    ___{mot}___ ")
+
+    
+
+
+
+
+print("   |:H:a:p:p:y:|\n __|___________|__\n|^^^^^^^^^^^^^^^^^|\n|:B:i:r:t:h:d:a:y:|\n|                 |\n~~~~~~~~~~~~~~~~~~~ ")
