ada254f4d771bb0cfaec6b08df37920026ebbfc4
unknown <ybouazzani@gmail.com>
2025-03-24 19:22:27 +0100

    autre methode pour exercice 7

--- Week3/Day2/ExerciceXp/Exercice7.js
+++ Week3/Day2/ExerciceXp/Exercice7.js
@@ -1,18 +1,12 @@
 const names = ["Jack", "Philip", "Sarah", "Amanda", "Bernard", "Kyle"];
 let mot = []
 for (i=0;i<names.length;i++){
-    
     let mots = names[i]
-
-    for (j=0 ; j<1 ; j++){
-
-        mot.push(mots[0])
-
-    }
-
+    mot.push(mots[0])
+    mot.sort()
 }
-console.log(mot.sort());
 console.log(mot.join(""));
 
+// autre methode
 
-
+console.log(names.map(name => name[0]).sort().join(""))
