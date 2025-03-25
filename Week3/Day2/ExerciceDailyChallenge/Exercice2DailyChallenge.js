bf4aa8f37756af8243a516ea27121340036517a2
unknown <ybouazzani@gmail.com>
2025-03-25 02:36:34 +0100

    exercice Xp daily challeng

---
+++ Week3/Day2/ExerciceDailyChallenge/Exercice2DailyChallenge.js
@@ -0,0 +1,25 @@
+let character = ""
+for(let i = 0 ;i<6 ;i++){
+    character += "* "
+    console.log(character);
+    
+}
+
+
+for(let i = 0 ;i<6 ;i++){
+    character = ''
+    
+    for(let j = 0 ;j<i ;j++){
+        
+
+        character += "* " 
+        
+        
+    }
+    console.log(character);
+    
+        
+
+}
+
+
