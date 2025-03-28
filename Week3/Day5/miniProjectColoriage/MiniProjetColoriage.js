a24cb2bf5c4fe4f130cb98c33a63160555b1a48f
unknown <ybouazzani@gmail.com>
2025-03-28 16:32:57 +0100

    quelque modification pour la button clear

--- Week3/Day5/miniProjectColoriage/MiniProjetColoriage.js
+++ Week3/Day5/miniProjectColoriage/MiniProjetColoriage.js
@@ -1,5 +1,5 @@
 const content = document.getElementById("content");
-let selector = "lightskyblue";
+let selector = "lightskylue";
 const color = document.querySelectorAll(".color");
 let bool = false
 const btn = document.getElementById('btn')
@@ -7,9 +7,7 @@ for (let i = 0; i < 1920; i++) {
   const div = document.createElement("div");
   div.classList.add("rightDiv");
   content.appendChild(div);
-  btn.addEventListener('click',function(){
-    div.style.background = 'white'
-  })
+
   div.addEventListener('mouseover',function(){
     if(bool){
       div.style.background = selectedColor
@@ -24,10 +22,19 @@ for (let i = 0; i < 1920; i++) {
     bool=false
   })
   };
+
+
 color.forEach((item) => {
   item.addEventListener('click' , function () {
     selectedColor = window.getComputedStyle(item).background
   })})
 
-
+  btn.addEventListener('click',function(){
+    const divs = document.querySelectorAll('.rightDiv');  
+    divs.forEach((div) => {
+      div.style.background = 'white';  
+    });
+    selectedColor = 'white'
+    
+  })
 
